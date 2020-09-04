253aa9087c762c0cc96fdfa575c0be6cc6bfe361
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-09-04 14:50:40 -0700

    Fixed bugs in uselt
    * Figwheel's REPL behaves a bit differently, so we need to hook *ns* instead of &env
    * This new approach attempts to get the current namespace from either *ns* or &env

--- src/longterm/core.clj
+++ src/longterm/core.clj
@@ -5,14 +5,18 @@
   [name args & code]
   `(defn ~name ~args ~@code))
 
-(defmacro use-longterm
+;; Use this macro at the top of a longterm code:
+;; (ns foo.bar (:require [longterm.core :as lt]))
+;; (lt/uselt)
+(defmacro uselt
   "Imports all the longterm macros and functions"
-  []
-  (if &env
-    (do
-      (print "^&env is" &env)
-      (let [current-namespace (symbol ((&env :ns) :name))]
-        (when-not (= current-namespace 'longterm.core)
-          `(~'ns ~current-namespace
-             (:require [~'longterm.core :refer-macros [~'defp]])))))
-    (print "&env is nil")))
+  ([ns-symbol]
+   `(~'ns ~ns-symbol (:require [longterm.core :refer-macros [~'defp]])))
+
+  ([]
+   (println ":name &env" (if &env (:name &env) "&env unbound"))
+   (println "ns-name *ns*" (if *ns* (ns-name *ns*) "*ns* unbound"))
+   (cond
+     *ns* `(uselt ~(symbol (ns-name *ns*)))
+     &env `(uselt ~(symbol (:name &env)))
+     :else (throw "Unable to determine current namespace"))))
