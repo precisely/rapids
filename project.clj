9cdf2d96aceca57d188811f0c7222cff854a862e
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-10-16 07:43:21 -0700

    Release 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject longterm "0.1.4-SNAPSHOT"
+(defproject longterm "0.1.4"
   :description "FIXME: write description"
   ; :url "http://example.com/FIXME"
   :license {:name "All Rights Reserved"
