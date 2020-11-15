0648f61507fa47674d71a9f6166b4b8fe74142aa
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-11-14 19:49:21 -0800

    Add expire-run!

---
+++ test/longterm/time_test.clj
@@ -0,0 +1,17 @@
+(ns ^:unit longterm.time_test
+  (:require [clojure.test :refer :all]
+            [java-time :as t]
+            [longterm.time :refer :all])
+  (:import (java.time LocalDateTime)))
+
+(deftest TimeTests
+  (testing "now produces the current local-date-time"
+    (let [current-time (now)]
+      (is (instance? LocalDateTime current-time))
+      (is (= (t/local-date? current-time)))))
+
+  (testing "from-now produces a local-date-time with the correct offset"
+    (let [current-time (now)
+          in-3-days (-> 3 days from-now)]
+      (is (= 3
+            (-> in-3-days .getDayOfMonth  (- (-> current-time .getDayOfMonth))))))))
