02717e62ef8a3f123032186a9dc8d9f7fff89449
Aneil Mallavarapu <aneilmallavar@gmail.com>
2021-12-01 20:58:39 -0800

    Bump to 0.6.4

--- project.clj
+++ project.clj
@@ -29,7 +29,7 @@
                       v]))
     (into {})))
 
-(defproject precisely/rapids "0.6.4-SNAPSHOT"
+(defproject precisely/rapids "0.6.4"
   :description "A Clojure DSL for scripting user flows"
   :url "https://github.com/precisely/rapids"
   :license {:name "All Rights Reserved"
