0ca49d886c5ffd3d55a25be13657cfc404ebee3c
Aneil Mallavarapu <aneilmallavar@gmail.com>
2022-03-09 02:56:16 -0800

    Remove duplicate and commented code

--- test/rapids/partitioner/partition_macroexpand_test.clj
+++ test/rapids/partitioner/partition_macroexpand_test.clj
@@ -10,7 +10,7 @@
 
 (deftest ^:unit partition-macroexpand-test
   (testing "partition macroexpander"
-    (testing "macroexpanding autosyms results in non-equal lists "
+    (testing "macroexpanding autosyms results in unequal values, but not with-gensym-context"
       (is (not=
             (macroexpand `(a#))
             (macroexpand `(a#))))
@@ -18,7 +18,7 @@
             (with-gensym-context (partition-macroexpand `(a#)))
             (with-gensym-context (partition-macroexpand `(a#))))))
 
-    (testing "macroexpanding gensyms results in non-equal lists "
+    (testing "macroexpanding the same macro results in unequal values, but not with-gensym-context"
       (is (not=
             (with-gensym-context (macroexpand `(test-macro)))
             (with-gensym-context (macroexpand `(test-macro)))))
@@ -26,11 +26,6 @@
             (with-gensym-context (partition-macroexpand `(test-macro)))
             (with-gensym-context (partition-macroexpand `(test-macro))))))
 
-    (testing "macroexpanding the same macro twice ought to produce different variables"
-      (with-gensym-context
-        (is (not=
-              (partition-macroexpand `(test-macro))
-              (partition-macroexpand `(test-macro))))))
     (testing "macroexpanding clojure core macros also results in identical code"
       (is (not=
             (macroexpand `(doseq [a 1] a))
@@ -38,15 +33,11 @@
       (is (=
             (with-gensym-context (partition-macroexpand `(doseq [a 1] a)))
             (with-gensym-context (partition-macroexpand `(doseq [a 1] a))))))
+
     (testing "metadata is preserved"
       (let [expr (with-meta '(and a b) {:column 123 :line 456})]
         (is (= (meta (with-gensym-context (partition-macroexpand expr)))
               {:column 123 :line 456}))))
-
-    (testing "expanding clojure.core macros works as expected"
-      (is (=
-            (with-gensym-context (partition-macroexpand '(doseq [a '(1 2 3)] (println a))))
-            (with-gensym-context (partition-macroexpand '(doseq [a '(1 2 3)] (println a)))))))
 
     (testing "gensym-replacement? detects our gensym-replacement symbols"
       (is (gensym-replacement? '<<123>>))
