10b8c602e4ca3d7759fab5cfbac2c294bd2abf23
Aneil Mallavarapu <aneilmallavar@gmail.com>
2022-06-10 09:48:00 -0700

    Add macroexpand-all to repl

--- src/rapids/support/repl.clj
+++ src/rapids/support/repl.clj
@@ -3,4 +3,5 @@
             [debux.core :refer :all]
             [rapids.support.debug :refer :all]
             [clojure.pprint :refer :all]
+            [clojure.walk :refer [macroexpand-all]]
             [rapids.support.util :refer :all]))
