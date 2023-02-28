d6377d607a01ac173016d7c017442f14429e015c
Aneil Mallavarapu <aneilmallavar@gmail.com>
2023-02-28 15:04:15 -0600

    Improve test coverage

--- test/rapids/support/util_test.clj
+++ test/rapids/support/util_test.clj
@@ -1,4 +1,4 @@
-(ns rapids.util_test
+(ns rapids.support.util_test
   (:require [clojure.test :refer :all]
             [rapids.support.util :refer :all]))
 
@@ -6,7 +6,7 @@
 (def a (Foo. 1))
 (defn is-foo? [x] (instance? Foo x))
 
-(deftest RefersTo
+(deftest refers-to-test
   (testing "Tests fully qualified symbol instance"
     (is (true? (refers-to? is-foo? `a))))
   (testing "Tests var instance"
@@ -14,7 +14,7 @@
   (testing "Testing instance directly"
     (is (true? (refers-to? is-foo? a)))))
 
-(deftest ReverseInterleave
+(deftest reverse-interleave-test
   (testing "two lists"
     (is (= '([:a :b :c] [1 2 3 ])
            (reverse-interleave '(:a 1 :b 2 :c 3) 2))))
@@ -23,7 +23,25 @@
     (is (= '([:a :b :c] [1 2 3] ["foo" "bar" "baz"])
            (reverse-interleave '(:a 1 "foo" :b 2 "bar" :c 3 "baz") 3)))))
 
-(deftest In
+(deftest in-test
   (testing "in?"
     (is (true? (in? [:a :b :c] :b)))
     (is (nil? (in? [:a :b :c] :d)))))
+
+(deftest dissoc-in-test
+  (testing "dissoc-in"
+    (let [input           {:a {:b {:c 1 :d 2} :e {:f {:g 3}}}}]
+      (is (= {:a {:b {:d 2}, :e {:f {:g 3}}}} (dissoc-in input [:a :b :c] ))))))
+
+(deftest sausage-to-snake-test
+  (is (= :my_cool_keyword (sausage-to-snake :my-cool-keyword))))
+
+(deftest snake-to-sausage-test
+  (is (= :my-cool-keyword (snake-to-sausage :my_cool_keyword))))
+
+(deftest atom?-test
+  (is (atom? (atom 1)))
+  (is (atom? (atom :hello)))
+  (is (not (atom? 1)))
+  (is (not (atom? :hello)))
+  (is (not (atom? []))))
