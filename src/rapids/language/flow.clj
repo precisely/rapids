4edabec6e62782a5c376c75787a383df8d754135
Aneil Mallavarapu <aneilmallavar@gmail.com>
2023-03-09 08:38:15 -0600

    Add tests for multiarity deflow
    Getting multiarity anonymous flows working is hard, so I left this as a TODO

--- src/rapids/language/flow.clj
+++ src/rapids/language/flow.clj
@@ -5,23 +5,26 @@
             [rapids.support.util :refer [qualify-symbol]]
             [rapids.partitioner.macroexpand :refer [with-gensym-context]]))
 
+(declare normalize-deflow-args)
 (defmacro deflow
   "Define a flow, using the same semantics as defn.
   Any doc-string or attrs are added to the var metadata. prepost-map defines a map with optional keys :pre and :post
   that contain collections of pre or post conditions."
   {:arglists '([name doc-string? attr-map? [params*] prepost-map? body]
                [name doc-string? attr-map? ([params*] prepost-map? body) + attr-map?])}
-  [name docstring? & fdecl]
+  [name & args]
   (with-gensym-context
     (with-flow-definitions name
-      (if-not (string? docstring?)
-        (with-meta `(deflow ~name "" ~docstring? ~@fdecl) (meta &form))
-        (let [qualified-name (qualify-symbol name)
-              address        (->address qualified-name)
-              [entry-fn-def, pmap] (partition-flow-body (meta &form) address fdecl)
-              flow-form      `(let [~'pfn-set ~(partition-map-def pmap)]
-                                (->Flow '~qualified-name, ~entry-fn-def, ~'pfn-set))]
-          `(def ^{:doc ~docstring?} ~name ~flow-form))))))
+      (let [[doc-string attr-map sigs] (normalize-deflow-args args)
+            meta-map       (merge (meta name) {:doc doc-string} attr-map)
+            qualified-name (qualify-symbol name)
+            address        (->address qualified-name)
+            [entry-fn-def, pmap] (partition-flow-body (meta &form) address sigs [] name)
+            flow-form      `(let [~'pfn-set ~(partition-map-def pmap)]
+                              (->Flow '~qualified-name, ~entry-fn-def, ~'pfn-set))]
+        `(do (def ~name ~flow-form)
+           (alter-meta! (var ~name) merge ~meta-map)
+           (var ~name))))))
 
 (defmacro flow
   "Special form for constructing anonymous flows. May only be invoked inside of deflow. Returns a Closure."
@@ -31,3 +34,24 @@
     (throw (ex-info "Invalid context: anonymous flow may only be defined inside of deflow."
              {:form &form}))))
 
+;;; Helpers
+
+(defn normalize-deflow-args
+  "Returns a vector of [doc-string attr-map sigs],
+  where sigs = (([] ...body), ([] ...body))."
+  [args]
+  (loop [[[test default] & rest-tests]
+         [[string? (constantly nil)]
+          [map? (constantly {})]
+          [(constantly false) (fn [arg rest-args]
+                               (cond
+                                 (vector? arg) (list (list* arg rest-args))
+                                 (list? arg) (list* arg rest-args)))]]
+         args    args
+         results []]
+    (let [[arg & rest-args] args]
+      (if test
+        (if (test arg)
+          (recur rest-tests rest-args (conj results arg))
+          (recur rest-tests args (conj results (default arg rest-args))))
+        results))))
